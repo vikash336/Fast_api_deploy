179a1eded89d401ed2b29887e879e6fcd09a4781
vikash336 <Vikashgusain1999@gmail.com>
2023-08-30 14:31:40 +0530

    your docker files

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,17 @@
-FROM python3.8.10
+FROM python:3.8.10
 
+# Set the working directory in the container
 WORKDIR /app
 
+
+COPY . /app/
+
+# Install the dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application's code into the container at /app
 COPY . /app
 
+EXPOSE 8000
 
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres:root@localhost/fastapi_microservice"
+DATABASE_URL = "postgresql://postgres:root@localhost:5432/fastapi_microservice"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
